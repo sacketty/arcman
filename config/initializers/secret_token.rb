bd1580e777afd7e5ca3d8b4471a605023f4e5d4e
Henri Sack <hsack@abriva.net>
2012-05-22 17:01:12 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcman::Application.config.secret_token = '7e4cb7693b01ce2fc815558ede7b935738ef02c98cbf89452a3145c09e9c5f7fd793a62e3079b2d74834fa35c846c894d52db67d3a1bcfeecca57bec5655478e'
